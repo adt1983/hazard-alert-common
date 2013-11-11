e68cc97979ad6806bf8239f0e92c4e69b79af5c0
adt1983 <adt1983@8036965c-fe87-45e5-bc55-c046631249d1>
2013-11-11 17:34:30 +0000

    implement Serializable Anti-Meridian helper functions

--- common/trunk/test/BoundsTest.java
+++ common/trunk/test/BoundsTest.java
@@ -1,10 +1,23 @@
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import org.apache.commons.lang3.SerializationUtils;
 import org.junit.Test;
+
+import com.hazardalert.common.Bounds;
 
 public class BoundsTest {
 	@Test
 	public void test() {
-		fail();
+		Bounds tests[] = new Bounds[] { new Bounds(1.0, 2.0, -1.0, -2.0) };
+		for (int i = 0; i < tests.length; i++) {
+			try {
+				Bounds clone = SerializationUtils.clone(tests[i]);
+				assertTrue(tests[i].equals(clone));
+			}
+			catch (Exception e) {
+				fail();
+			}
+		}
 	}
 }
