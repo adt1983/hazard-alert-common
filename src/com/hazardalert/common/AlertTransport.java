e68c893f0f9a87f96e86943ccf66378338b6874f
adt1983 <adt1983@8036965c-fe87-45e5-bc55-c046631249d1>
2013-11-11 17:34:56 +0000

    +sourceUrl

--- src/com/hazardalert/common/AlertTransport.java
+++ src/com/hazardalert/common/AlertTransport.java
@@ -1,7 +1,18 @@
 package com.hazardalert.common;
 
 public class AlertTransport {
+	public String sourceUrl;
+
 	public String payload; // Base 64 encoded XML from javax.xml.bind.DatatypeConverter.printBase64Binary
+
+	public String getSourceUrl() {
+		return sourceUrl;
+	}
+
+	public AlertTransport setSourceUrl(String sourceUrl) {
+		this.sourceUrl = sourceUrl;
+		return this;
+	}
 
 	public String getPayload() {
 		return payload;
